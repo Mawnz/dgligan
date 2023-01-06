a2b41501845a64e3d9733a092afd6b9d93cab68f
mans <mans.aberg@gmail.com>
2023-01-06 13:29:31 +0100

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,60 @@
 <template>
   <div class="h-full container flex-col">
-    <section id="overview"></section>
+    <section id="overview">
+      <table>
+        <tr>
+          <th>
+            Namn
+          </th>
+          <th>
+            Totalt spelade hål
+          </th>
+          <th>
+            Totalt antal kast
+          </th>
+          <th>
+            Kast per hål (snitt)
+          </th>
+          <th>
+            Total möjlig par
+          </th>
+          <th>
+            Total spelad par
+          </th>
+          <th>
+            Antal birdie
+          </th>
+          <th>
+            Antal par
+          </th>
+          <th>
+            Antal boogie
+          </th>
+          <th>
+            Antal > +1
+          </th>
+        </tr>
+        <tbody>
+          <tr v-for="player in players" :key="player.name">
+            <td>
+              {{ player.name }}
+            </td>
+            <td>
+              {{ calcPlayerTotalPlayedHoles(player) }}
+            </td>
+            <td>
+              {{ calcPlayerTotalThrows(player) }}
+            </td>
+            <td>
+              {{ Math.round((calcPlayerTotalThrows(player) / calcPlayerTotalPlayedHoles(player)) * 100) / 100 }}
+            </td>
+            <td>
+              {{ calcPlayerParTotal(player) }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   </div>
 </template>
 
@@ -21,9 +75,15 @@ interface Score {
 
 interface Competition {
   name: string;
-  holes: { [key: number]: Hole };
-  scores: { [key: string]: Score[] };
+  holes?: { [key: number]: Hole };
+  scores?: { [key: string]: Score[] };
+  playerScores?: Score[];
   date?: string;
+}
+
+interface Player {
+  competitions: { [key:string]: Competition };
+  name: string;
 }
 
 export default defineComponent({
@@ -32,72 +92,136 @@ export default defineComponent({
       comps: {} as { [key: string]: Competition },
     };
   },
-  async created() {
-    const raw = await useFetch(
-      "https://docs.google.com/spreadsheets/d/1U8vlST4Y66HoIdv2KtQHh_GagpLVheFBsQRQ_uCP7Nk/gviz/tq?tqx=out:csv&sheet=raw"
-    );
-
-    const meta = await useFetch(
+  created() {
+    useFetch(
       "https://docs.google.com/spreadsheets/d/1U8vlST4Y66HoIdv2KtQHh_GagpLVheFBsQRQ_uCP7Nk/gviz/tq?tqx=out:csv&sheet=meta"
-    );
+    ).then((meta) => {
+      let i = 0;
 
-    let i = 0;
+      for (let row of (meta.data.value as string).split("\n")) {
+        if (i === 0) {
+          i += 1;
+        } else {
+          const [name, date] = row.replaceAll('"', "").split(",");
 
-    for (let row of (meta.data.value as string).split("\n")) {
-      if (i === 0) {
-        i += 1;
-      } else {
-        const [name, date] = row.replaceAll('"', "").split(",");
-
-        if (!this.comps[name]) {
-          this.comps[name] = {
-            name,
-            holes: {},
-            scores: {},
-            date,
-          };
+          if (!this.comps[name]) {
+            this.comps[name] = {
+              name,
+              holes: {},
+              scores: {},
+              date,
+            };
+          }
         }
       }
-    }
+      useFetch(
+        "https://docs.google.com/spreadsheets/d/1U8vlST4Y66HoIdv2KtQHh_GagpLVheFBsQRQ_uCP7Nk/gviz/tq?tqx=out:csv&sheet=raw"
+      ).then((raw) => {
+        i = 0;
 
-    i = 0;
+        for (let row of (raw.data.value as string).split("\n")) {
+          if (i === 0) {
+            i += 1;
+          } else {
+            const [player, comp, h, hp, s] = row.replaceAll('"', "").split(",");
+            const hole = parseInt(h);
+            const holepar = parseInt(hp);
+            const score = parseInt(s);
 
-    for (let row of (raw.data.value as string).split("\n")) {
-      if (i === 0) {
-        i += 1;
-      } else {
-        const [player, comp, h, hp, s] = row.replaceAll('"', "").split(",");
-        const hole = parseInt(h);
-        const holepar = parseInt(hp);
-        const score = parseInt(s);
+            if (!this.comps[comp]) {
+              this.comps[comp] = {
+                name: comp,
+                holes: {},
+                scores: {},
+              };
+            }
 
-        if (!this.comps[comp]) {
-          this.comps[comp] = {
-            name: comp,
-            holes: {},
-            scores: {},
-          };
+            if (!this.comps[comp].scores[player]) {
+              this.comps[comp].scores[player] = [];
+            }
+
+            this.comps[comp].scores[player].push({
+              hole,
+              par: holepar,
+              score,
+              player,
+            });
+
+            this.comps[comp].holes[hole] = { number: hole, par: holepar };
+          }
         }
-
-        if (!this.comps[comp].scores[player]) {
-          this.comps[comp].scores[player] = [];
-        }
-
-        this.comps[comp].scores[player].push({
-          hole,
-          par: holepar,
-          score,
-          player,
-        });
-
-        this.comps[comp].holes[hole] = { number: hole, par: holepar };
+      });
+    });
+  },
+  methods: {
+    calcPlayerTotalPlayedHoles(player: Player): number {
+      let total = 0;
+      let comps = player.competitions;
+      for(let c in comps) {
+        total += comps[c].playerScores!.length
       }
+      return total;
+    },
+    calcPlayerTotalThrows(player: Player): number {
+      let total = 0;
+      let comps = player.competitions;
+      for(let c in comps) {
+        let throws = comps[c].playerScores!.reduce((sum, { score }) => sum + score, 0)
+        total += throws;
+      }
+      return total;
+    },
+    calcPlayerParTotal(player: Player): number {
+      let total = 0;
+      let comps = player.competitions;
+      for(let c in comps) {
+        let diff = comps[c].playerScores!.reduce((sum, { score }) => sum + score, 0) - comps[c].playerScores!.reduce((su, { par })  => su + par, 0);
+        total += diff;
+      }
+
+      return total;
+    },
+      calcPlayerAvgThrows(comps: Competition[]): number {
+      let avg = 0;
+      let playerHolesTotal = 0;
+      for(let c of comps) {
+        avg += 1;
+      }
+
+      return avg;
     }
   },
   computed: {
-    competitions() {
+    competitions(): Competition[] {
       return Object.values(this.comps);
     },
+    players(): Player[] {
+      const players = Object.values(this.comps).reduce((players, comp) => {
+        for(let player in comp.scores) {
+          if(!players.hasOwnProperty(player)) {
+            players[player] = {
+              name: player,
+              competitions: {}
+            }
+          }
+          if(!players[player].competitions.hasOwnProperty(comp.name)) {
+            players[player].competitions[comp.name] = {
+              name: comp.name, playerScores: comp.scores[player]
+            }
+          }
+        }
+
+        return players;
+      }, {} as { [key:string]: Player });
+
+      return Object.values(players);
+    },
+    worst() {
+      for(let player of this.players) {
+        console.log(player.name, this.calcPlayerAvgPar(player.competitions))
+      }
+      return 'me'
+    }
   },
 });
 </script>
@@ -106,4 +230,13 @@ export default defineComponent({
 #overview {
   backgorund-color: red;
 }
+
+table, th, td {
+  border: solid 1px;
+}
+
+th, td {
+  padding: 5px;
+}
+
 </style>
